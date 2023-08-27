890085847df4f80ec47dc604bb291939f94c74c2
Yuqi Xiao <a1849716@adelaide.edu.au>
2023-08-27 12:40:54 +0930

    save

---
+++ assignment1/main.cpp
@@ -0,0 +1,65 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+using namespace std;
+
+string school_add(string n1, string n2, int base) {
+  // reverse strings
+  reverse(n1.begin(), n1.end());
+  reverse(n2.begin(), n2.end());
+
+  // Get larger of two numbers (n1 is larger of the two)
+  if (n1.length() < n2.length()) {
+    swap(n1, n2);
+  }
+
+  // get difference and adding zero to smaller
+  int difference = n1.length() - n2.length();
+  for (int i = 0; i < difference; i++) {
+    n2.push_back('0');
+  }
+
+  // for loop to add individual numbers
+  string sum;
+  int carry = 0;
+  
+  for (int i = 0; i < n1.length(); i++) {
+    int curr_sum = (n1[i] - '0') + (n2[i] - '0') + carry;
+    carry = 0;
+    if (curr_sum >= base && i != (n1.length() - 1)) {
+      carry = 1;
+      curr_sum = curr_sum % base;
+    } else {
+      carry = 0;
+    }
+
+    if (curr_sum == 2 && base == 2){
+      curr_sum = 10;
+    }
+    sum = to_string(curr_sum) + sum;
+  }
+
+  return sum;
+};
+
+int karatsuba_mult(int n1, int n2, int base) {
+  int num1Count = log10(n1) + 1;
+  int num2Count = log10(n2) + 1;
+  int carry = 0;
+  int prod = 0;
+  if (num1Count == 1 && num2Count == 1 && base == 10) {
+    prod = n1 * n2;
+    return prod;
+  }
+};
+
+int main() {
+  string n1, n2;
+  int base = 0;
+  cout << "Sample input: ";
+  cin >> n1 >> n2 >> base;
+  string sum = school_add(n1, n2, base);
+  // int prod = karatsuba_mult(n1, n2, base);
+  //  cout << n1 << " " << n2 << " " << 0 << "\n";
+  cout << sum << " " << 0 << " " << 0 << "\n";
+}
