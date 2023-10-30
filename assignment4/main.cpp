e52adb8650bf7881c59a9155286db7573bd0142b
a1849716 <109998412+a1849716@users.noreply.github.com>
2023-10-30 08:36:29 +0000

    save

--- assignment4/main.cpp
+++ assignment4/main.cpp
@@ -3,67 +3,19 @@
 #include <vector>
 using namespace std;
 
-struct road {
-  int i, j, cost;
-  bool exist;
-  road(int i, int j, int cost, bool exist) {
-    this->i;
-    this->j;
-    this->cost;
-    this->exist;
-  }
-};
-class Union_Find {
- public:
-  int parent[40], seniority[40];
+int minCost() {}
 
-  int find(int i) {
-    if (parent[i] != i) {
-      parent[i] = find(parent[i]);
-    } else {
-      return i;
-    }
-  }
+int prim(vector<int>){
 
-  void link(int i, int j) {
-    if (seniority[i] < seniority[j])
-      parent[i] = j;
-    else {
-      parent[j] = i;
-      if (seniority[i] == seniority[j]) {
-        seniority[i]++;
-      }
-    }
-  }
-
-  bool Union(int i, int j) {
-    int init_i = find(i);
-    int init_j = find(j);
-
-    if (init_i != init_j) {
-      link(init_i, init_j);
-      return true;
-    }
-    return false;
-  }
 };
 
-int find_cost(char str) {
-  int temp = str;
-  if (temp >= 97) {
-    return temp - 97 + 26;
-  } else if (temp <= 90) {
-    return temp - 66 + 26;
-  }
-}
-
-vector<vector<char>> parseChar(string& str) {
-  vector<vector<char>> array;
+vector<vector<char> > parseChar(string& str) {
+  vector<vector<char> > array;
   stringstream ss(str);
   string temp;
-
+  int i = 0;
   while (getline(ss, temp, ',')) {
-    vector<char> row;
+    vector<char> row(temp.begin(), temp.end());
     array.push_back(row);
   }
   return array;
@@ -74,11 +26,52 @@ void input(string str) {
   stringstream ss(str);
   string cities, build, destroy;
   getline(ss, cities, ' ');
+  cout << cities << endl;
   getline(ss, build, ' ');
+  cout << build << endl;
   getline(ss, destroy, ' ');
-  vector<vector<char>> city = parseChar(cities);
-  vector<vector<char>> buildCost = parseChar(build);
-  vector<vector<char>> destroyCost = parseChar(destroy);
+  cout << destroy << endl;
+  vector<vector<char> > city = parseChar(cities);
+  vector<vector<char> > buildCost = parseChar(build);
+  vector<vector<char> > destroyCost = parseChar(destroy);
+  /*
+  int index = 0;
+  cout << "Build Matrix:" << endl;
+  for (const auto& row : city) {
+    cout << "row " << index << " :";
+    for (char val : row) {
+      cout << val << ' ';
+    }
+    index++;
+    cout << endl;
+  }
+
+  index = 0;
+  cout << "Build Matrix:" << endl;
+  for (const auto& row : buildCost) {
+    cout << "row " << index << " :";
+    for (char val : row) {
+      cout << val << ' ';
+    }
+    index++;
+    cout << endl;
+  }
+
+  index = 0;
+  cout << "Build Matrix:" << endl;
+  for (const auto& row : destroyCost) {
+    cout << "row " << index << " :";
+    for (char val : row) {
+      cout << val << ' ';
+    }
+    index++;
+    cout << endl;
+  }
+  */
 }
 
-int main() {}
+int main() {
+  string cmd;
+  getline(cin, cmd);
+  input(cmd);
+}
